f489c277af8b6264e6303a57a0305da66f472a12
Nickp1993 <61044930+Nickp1993@users.noreply.github.com>
2021-09-27 14:08:40 +0200

    Update __init__.py

--- PyChi/__init__.py
+++ PyChi/__init__.py
@@ -3,3 +3,9 @@ ver = "20210927"
 print(f"PyChi version {ver} imported succesfully.\n ")
 from core.channel import Channel
 from core.environment import Environment, process, selected
+
+from math import *
+from dataclasses import dataclass
+from numpy import random
+import numpy
+numpy.seterr(divide='ignore')
