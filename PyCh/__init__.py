bdb6d388ed199455080a7c6efa321d463104fdc5
Nickp1993 <61044930+Nickp1993@users.noreply.github.com>
2021-09-29 10:56:05 +0200

    Update and rename PyChi/__init__.py to PyCh/__init__.py

--- PyCh/__init__.py
+++ PyCh/__init__.py
@@ -1,11 +1,13 @@
 #! usr/bin/python3
 ver = "20210927"
 print(f"PyChi version {ver} imported succesfully.\n ")
-from core.channel import Channel
-from core.environment import Environment, process, selected
+from channel import Channel
+from environment import Environment, process, selected
 
 from math import *
 from dataclasses import dataclass
 from numpy import random
+
 import numpy
 numpy.seterr(divide='ignore')
+import matplotlib.pyplot as plt
