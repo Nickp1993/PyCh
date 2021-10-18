1202f85428e3ad4da026de58021fbaf3b8d9e3a5
Nick Paape <n.paape@tue.nl>
2021-10-18 10:24:58 +0200

    - Added figures for the assignment - Added live plots

--- src/PyCh/__init__.py
+++ src/PyCh/__init__.py
@@ -1,12 +1,27 @@
 #! usr/bin/python3
 ver = "0.2"
-print(f"PyCh version {ver} imported succesfully.\n ")
+# ===================================
+# import core
+# ===================================
 from .core.channel import Channel, Communicator, Sender, Receiver
 from .core.environment import Environment, process, selected
+
+# ===================================
+# import math utilities
+# ===================================
 from math import *
 from dataclasses import dataclass
 from numpy import random
-
 import numpy
 numpy.seterr(divide='ignore')
-# import matplotlib.pyplot as plt
+
+# ===================================
+# import plot utilities
+# ===================================
+import matplotlib
+from matplotlib import pyplot
+matplotlib.use('nbagg')
+from .utilities.liveplot import LivePlot, LiveStepPlot
+
+# Finished
+print(f"PyCh version {ver} imported succesfully.\n ")
