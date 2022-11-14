6bb69b703e9959dfd83927b2e77d006f81f2e75c
Nickp1993 <61044930+Nickp1993@users.noreply.github.com>
2022-11-14 10:38:58 +0100

    Update __init__.py

--- src/PyCh/__init__.py
+++ src/PyCh/__init__.py
@@ -22,7 +22,7 @@ import matplotlib
 from matplotlib import pyplot as plt
 matplotlib.use('nbagg')
 from .utilities.liveplot import LivePlot, LiveStepPlot
-from .utilies.draw_lot_time_diagram import draw_lot_time_diagram
+from .utilities.draw_lot_time_diagram import draw_lot_time_diagram
 
 # Finished
 print(f"PyCh version {ver} imported succesfully.\n ")
